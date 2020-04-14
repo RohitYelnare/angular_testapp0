47a5d97f95e66fe499a4d4fe1dd4e146437582b0
RohitYelnare <ryelnare@gmail.com>
2020-04-14 23:19:42 +0530

    1 file modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -9,7 +9,7 @@ export class TestComponent implements OnInit {
   public nameboxid="hi";
   public text="";
   appear(a,b){
-    this.text="Your full name is " + a + " " +b;
+    this.text="Your full name is " + a + " " + b;
   }
   disappear(){
     this.text='';
