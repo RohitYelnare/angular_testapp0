e7b34ab628e7808c73dedaa03578ee07627fcb43
RohitYelnare <ryelnare@gmail.com>
2020-04-15 01:00:44 +0530

    2 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -6,13 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.css']
 })
 export class TestComponent implements OnInit {
-  public nameboxid="hi";
   public text="";
+  public text2="";
+  public firstname="";
+  public lastname="";
+  public counter=0;
+  public button_content="Show name"
   appear(a,b){
-    this.text="Your full name is " + a + " " + b;
+    this.text = a + " " + b;
   }
   disappear(){
     this.text='';
+  }
+  onClick(a,b,c){
+    this.counter++;
+    if(this.counter%2==0){
+      this.text2= a + " " + b;
+      this.button_content="Hide name";
+    }
+    else{
+      this.text2="";
+      this.button_content="Show name";
+    }
   }
    constructor() { }
 
