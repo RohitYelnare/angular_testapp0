8c2d2b34f396acd14479a7214a56329882b7402e
RohitYelnare <ryelnare@gmail.com>
2020-04-17 21:21:46 +0530

    2 files modified

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -11,7 +11,9 @@ export class TestComponent implements OnInit {
   public firstname="";
   public lastname="";
   public counter=0;
-  public button_content="Show name"
+  public button_content="Show name";
+  public myvar;
+  public myboolean="";
   appear(a,b){
     this.text = a + " " + b;
   }
@@ -28,6 +30,26 @@ export class TestComponent implements OnInit {
       this.button_content="Show name";
     }
     this.counter++;
+  }
+  msg(value){
+    if(value==1){
+    this.myboolean="true";
+    }
+    else{
+    this.myboolean="false";
+    }
+  }
+  myclick(value){
+    this.myvar=value;
+  }
+  mybool(){
+
+  }
+  truefunc(){
+    this.myboolean="true";
+  }
+  falsefunc(){
+    this.myboolean="false";
   }
    constructor() { }
 
